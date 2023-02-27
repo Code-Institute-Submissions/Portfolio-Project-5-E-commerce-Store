0c4e2ed8c0cfd09a14c8326b1a863cb074da20a7
Joha-will <johawilsonatu@icloud.com>
2023-02-27 17:08:00 +0000

    Customized Products and Category model admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Product
+
+
+class CategoryAdmin(admin.ModelAdmin):
+
+    list_display = ('category_name', 'slug',)
+
+    prepopulated_fields = {'slug': ('category_name',)}
+
+
+class ProductAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'description', 'slug', 'size', 'image', 'price',)
+
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
