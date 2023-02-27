419ae6b95a914f1d33d456504e6e446663696d99
Joha-will <johawilsonatu@icloud.com>
2023-02-27 16:15:57 +0000

    Created Category and Products models, then made migrations

--- products/models.py
+++ products/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+
+    category_name = models.CharField(max_length=254, db_index=True)
+
+    slug = models.SlugField(max_length=254, unique=True)
+
+    class Meta:
+
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+
+        return self.category_name
+
+
+class Product(models.Model):
+
+    name = models.CharField(max_length=254)
+
+    description = models.TextField(blank=True)
+
+    slug = models.SlugField(max_length=254)
+
+    size = models.CharField(max_length=5)
+
+    image = models.ImageField(null=True, blank=True)
+
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    date = models.DateField(auto_now_add=True)
+
+    class Meta:
+
+        verbose_name_plural = "products"
+
+    def __str__(self):
+
+        return self.name
