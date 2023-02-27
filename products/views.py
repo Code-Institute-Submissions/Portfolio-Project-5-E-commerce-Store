ac040ed5108adb74e6f849cbb4a28747c2a48164
Joha-will <johawilsonatu@icloud.com>
2023-02-27 18:49:41 +0000

    Added products views and created temlates

--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Product
+
+
+def all_products(request):
+    """ A view that displays all product to customers"""
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, "products/products.html", context)
