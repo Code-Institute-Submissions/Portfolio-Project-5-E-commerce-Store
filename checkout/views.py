91d46ca95be5ce41f7492c5d35d45a872cf02722
Joha-will <johawilsonatu@icloud.com>
2023-04-02 14:51:18 +0000

    Added more functionality to checkout view

--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 
-from .forms import OrderForm
+from .forms import OrderForm, Order
 
 from django.contrib import messages
 
@@ -10,6 +10,10 @@ import stripe
 
 from django.conf import settings
 
+from .models import OrderItem
+
+from products.models import Product
+
 
 def checkout(request):
     """ A view that renders the checkout page """
@@ -18,25 +22,91 @@ def checkout(request):
 
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    basket = request.session.get('basket', {})
+    if request.method == 'POST':
 
-    if not basket:
-        messages.error(request, " There's nothing in your basket!")
-        return redirect(reverse('products'))
+        basket = request.session.get('basket', {})
 
-    current_basket = basket_contents(request)
-    total_amount = current_basket['order_total']
-    stripe_total = round(total_amount * 100)
+        form_data = {
 
-    stripe.api_key = stripe_secret_key
+            'full_name': request.POST['full_name'],
 
-    intent = stripe.PaymentIntent.create(
+            'email': request.POST['email'],
 
-        amount=stripe_total,
+            'phone_number': request.POST['phone_number'],
 
-        currency=settings.STRIPE_CURRENCY,
+            'address1': request.POST['address1'],
 
-    )
+            'address2': request.POST['address2'],
+
+            'city': request.POST['city'],
+
+            'county': request.POST['county'],
+
+            'country': request.POST['country'],
+
+            'postal_code': request.POST['postal_code'],
+        }
+
+        order_form = OrderForm(form_data)
+
+        if order_form.is_valid():
+
+            order = order_form.save()
+
+            for product_id, product_quantity in basket.items():
+
+                try:
+
+                    product = get_object_or_404(Product, id=product_id)
+
+                    order_item = OrderItem(
+
+                        order=order,
+
+                        quantity=quantity,
+
+                        product=product,
+
+                    )
+                except Product.DoesNotExit:
+
+                    messages.error(request, " We're Sorry! One of the product \
+                         in your basket was not found in our database, please \
+                        contact for help!")
+
+                    order.delete()
+
+                    return redirect(reverse('view_basket'))
+
+            request.session['save-address'] = 'save-address' in request.POST
+
+            return redirect(reverse('checkout_success', args=[order.order_number]))
+
+        else:
+            messages.error(request, "There was an error when submiting the\
+             form. Please check the details you have provided. ")
+
+    else:
+
+        basket = request.session.get('basket', {})
+
+        if not basket:
+            messages.error(request, " There's nothing in your basket!")
+            return redirect(reverse('products'))
+
+        current_basket = basket_contents(request)
+        total_amount = current_basket['order_total']
+        stripe_total = round(total_amount * 100)
+
+        stripe.api_key = stripe_secret_key
+
+        intent = stripe.PaymentIntent.create(
+
+            amount=stripe_total,
+
+            currency=settings.STRIPE_CURRENCY,
+
+        )
     if not stripe_public_key:
         messages.warning(request, "Stripe Public key missing, did you forget \
              to set variable?")
@@ -50,3 +120,9 @@ def checkout(request):
     }
 
     return render(request, 'checkout/checkout.html', context)
+
+
+def checkout_success(request):
+    """ This view is used for successful checkouts"""
+
+    save_address = 
