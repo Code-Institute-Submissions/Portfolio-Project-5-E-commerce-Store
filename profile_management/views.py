a27a33c6722bcb234a6479bce79f69ec4b81e390
Joha-will <johawilsonatu@icloud.com>
2023-04-05 21:56:29 +0000

    Added profile views and updated template

--- profile_management/views.py
+++ profile_management/views.py
@@ -4,12 +4,24 @@ from .models import UserProfile
 
 from .forms import UserProfileForm
 
+from django.contrib import messages
+
 
 def view_profile(request):
 
     """A view that renders the user profile"""
 
     profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+
+        form = UserProfileForm(request.POST, instance=profile)
+
+        if form.is_valid():
+
+            form.save()
+
+            messages.info(request, " Profile updated succefully")
 
     form = UserProfileForm(instance=profile)
 
