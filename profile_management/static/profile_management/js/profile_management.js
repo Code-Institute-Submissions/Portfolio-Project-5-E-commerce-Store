3d43c8c691273629af3beaa4d603824a6a8359d3
Joha-will <johawilsonatu@icloud.com>
2023-04-05 21:35:19 +0000

    Added country selected color change for profile management template

---
+++ profile_management/static/profile_management/js/profile_management.js
@@ -0,0 +1,13 @@
+let countrySelected = $('#id_default_country').val();
+if (!countrySelected) {
+    $('#id_default_country').css('color', '#CFCFCF');
+};
+
+$('#id_default_country').change(function() {
+    countrySelected = $(this).val();
+    if(!countrySelected) {
+        $(this).css('color', '#CFCFCF');
+    } else {
+        $(this).css('color', 'black');
+    }
+})
