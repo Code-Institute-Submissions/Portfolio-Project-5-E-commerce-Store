3d58c4e3f6f45427de2afbfbc48b695331a98d46
Joha-will <johawilsonatu@icloud.com>
2023-04-05 16:06:34 +0000

    Created profile_management app and connected url, templates and created a userprofile model

--- profile_management/views.py
+++ profile_management/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def view_profile(request):
+
+    """A view that renders the user profile"""
+
+    template = 'profile_management/view_profile.html'
+
+    context = {
+
+    }
+
+    return render(request, template, context)
