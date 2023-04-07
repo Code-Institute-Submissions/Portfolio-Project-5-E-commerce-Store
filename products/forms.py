81a3072ccd3be88b6a327de3805be85a223a6bf0
Joha-will <>
2023-04-07 18:04:57 +0000

    Added add_product views, url and template

--- products/forms.py
+++ products/forms.py
@@ -11,7 +11,7 @@ class ProductForm(forms.ModelForm):
 
         fields = '__all__'
 
-        exclude = ('date')
+        exclude = ('date',)
 
     def __init__(self, *args, **kwargs):
 
--- products/urls.py
+++ products/urls.py
@@ -1,12 +1,15 @@
 
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
 
     path('', views.all_products, name='products'),
 
-    path('<product_slug>/', views.product_detail, name='product_detail'),
+    path('<int:product_slug>/', views.product_detail, name='product_detail'),
+
+    path('add_product/', views.add_product, name='add_product'),
 
     path('category/<category_slug>/', views.search_category, name='search_category'),
 
