a0ff9a877746f904f6e5e491c78d814a7ef43c73
Joha-will <johawilsonatu@icloud.com>
2023-04-13 03:46:08 +0000

    Added AWS config to settings@ @

---
+++ custom_storages.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+
+from storages.backends.s3boto3 import S3Boto3Storage
+
+class StaticStorage(S3Boto3Storage):
+
+    location = settins.STATICFILES_LOCATION
+
+
+class MediaStorage(S3Boto3Storage):
+
+    location = settins.MEDIAFILES_LOCATION
+
+     
--- ecommerce_store/settings.py
+++ ecommerce_store/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    
+    'storages',
 
     # Home App
     'home',
@@ -204,6 +206,34 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# S3 Bucket config
+if 'USE_AWS' in os.environ:
+
+    AWS_STORAGE_BUCKET_NAME = 'aduj-visuals'
+
+    AWS_S3_REGION_NAME = 'eu-west-2'
+
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    # Static and media files
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+
+    STATICFILES_LOCATION = 'static'
+
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+
+    MEDIAFILES_LOCATION = 'media'
+
+    # Override static and media URLS in production
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+
 
 
 # Default primary key field type
