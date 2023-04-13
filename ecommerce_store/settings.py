accea857fc7d759c9e71a59ef43b5b404a2a5d40
Joha-will <johawilsonatu@icloud.com>
2023-04-13 04:24:12 +0000

    Added cache control@

--- ecommerce_store/settings.py
+++ ecommerce_store/settings.py
@@ -215,6 +215,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # S3 Bucket config
 if 'USE_AWS' in os.environ:
 
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GTM',
+        'CacheControl': 'max-age=94608000',
+    }
+
     AWS_STORAGE_BUCKET_NAME = 'aduj-visuals'
 
     AWS_S3_REGION_NAME = 'eu-west-2'
