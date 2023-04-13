9f03fdb0033befd4bc08344ab5d2565ab84b5b70
Joha-will <johawilsonatu@icloud.com>
2023-04-13 01:09:29 +0000

    Added setting for database and Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn aduj-visuals.wsgi:application
--- ecommerce_store/settings.py
+++ ecommerce_store/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 
+import dj_database_url
+
 from pathlib import Path
 
 if os.path.isfile('env.py'):
@@ -29,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-johawill-portfolioproje-otuim5r4qxn.ws-eu93.gitpod.io', 'localhost']
+ALLOWED_HOSTS = ['aduj-visuals.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -147,12 +149,18 @@ WSGI_APPLICATION = 'ecommerce_store.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
 
 
 # Password validation
